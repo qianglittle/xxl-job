4a3726fc05e8498332e485ec80a534945cf3ca54
qianglittle <qiang.little@live.com>
2019-09-21 18:58:59 +0800

    修改读取配置文件方式

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -2,11 +2,13 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
 @SpringBootApplication
+@PropertySource(value = {"file:/project/properties/application.properties"})
 public class XxlJobAdminApplication {
 
 	public static void main(String[] args) {
